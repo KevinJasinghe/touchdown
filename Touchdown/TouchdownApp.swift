aa4467362e569160a9e9ef7c9f2665d76f2e5eed
KevinJasinghe <kevinjasinghe@gmail.com>
2022-02-25 16:05:20 -0500

    Initial Commit

---
+++ Touchdown/TouchdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchdownApp.swift
+//  Touchdown
+//
+//  Created by Kevin Jasinghe on 2022-02-25.
+//
+
+import SwiftUI
+
+@main
+struct TouchdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
